9b6bc1f2940f35966d698e30353ad4fea146d012
tejaswinihkumbar <213469082+tejaswinihkumbar@users.noreply.github.com>
2025-08-04 23:38:56 +0530

    backup

--- send_notification.php
+++ send_notification.php
@@ -3,9 +3,9 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Twilio\Rest\Client;
 
-$sid = 'AC2b732ee91f93cf6f53fde036cbdb3df8';
-$token = 'b6035f319b955168605fe690c72bb5ee';
-$twilioNumber = '+17163335712';
+$sid = "YOUR_TWILIO_ACCOUNT_SID";
+$token = "YOUR_TWILIO_AUTH_TOKEN";
+$twilioNumber = "YOUR_TWILIO_NUMBER";
 
 $client = new Client($sid, $token);
 
